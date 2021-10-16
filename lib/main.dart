1048ad7bc0e0344320bd17b77ed3860fc2c2756a
hugobiareis <hugobiareis@gmail.com>
2021-10-15 22:22:38 -0300

    gridview clickavel

--- lib/main.dart
+++ lib/main.dart
@@ -173,21 +173,83 @@ class _TelaHomeState extends State<TelaHome> {
         scrollDirection: Axis.vertical,
         itemCount: lista.length,
         itemBuilder: (context, index) {
-          return Card(
-            elevation: 30,
-            shadowColor: Colors.grey.shade500,
-            child: GridTile(
-              header: Text(
-                lista[index],
-                textAlign: TextAlign.center,
+          return GestureDetector(
+              child: Card(
+                elevation: 30,
+                shadowColor: Colors.grey.shade500,
+                child: GridTile(
+                  header: Text(
+                    lista[index],
+                    textAlign: TextAlign.center,
+                  ),
+                  child: Icon(Icons.card_giftcard_outlined, size: 100),
+                  footer: Text(
+                    descr[index],
+                    textAlign: TextAlign.center,
+                  ),
+                ),
               ),
-              child: Icon(Icons.card_giftcard_outlined, size: 150),
-              footer: Text(
-                descr[index],
-                textAlign: TextAlign.center,
-              ),
-            ),
-          );
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('DETALHES DO PRODUTO'),
+                        content: SizedBox(
+                          height: MediaQuery.of(context).size.height / 2,
+                          child: Column(
+                            children: [
+                              //
+                              //CAMPO DE TEXTO E BOTAO DE ESQUECI SENHA
+                              //
+                              Icon(Icons.card_giftcard_outlined, size: 150),
+                              Text(
+                                descr[index],
+                                textAlign: TextAlign.center,
+                              ),
+                              //BOTÃO ADICIONAR AO CARRINHO
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    primary: Colors.grey.shade500),
+                                onPressed: () {
+                                  setState(() {
+                                    //
+                                    //ENVIAR DADOS PARA CARRINHO E FECHAR
+                                    //
+                                    Navigator.of(context).pop();
+                                    ScaffoldMessenger.of(context)
+                                        .showSnackBar(SnackBar(
+                                      content: Text(
+                                        'ADICIONADO AO CARRINHO',
+                                        textAlign: TextAlign.center,
+                                      ),
+                                      duration: Duration(seconds: 4),
+                                    ));
+                                  });
+                                },
+                                child: Text('ADICIONAR AO CARRINHO',
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold)),
+                              ),
+                            ],
+                          ),
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text(
+                              'FECHAR',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          )
+                        ],
+                      );
+                    });
+              });
         },
       ),
     );
